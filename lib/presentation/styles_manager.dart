caa88d0b505c4e9701d586b722749f789bd94064
Yashraj0405 <yashrajpatil.co.in@gmail.com>
2023-07-19 17:40:04 +0530

    Added Styles Manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,81 @@
+import 'package:clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fontWeight: fontWeight,
+    color: color,
+  );
+}
+
+//Regular Text Style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.regular,
+    color,
+  );
+}
+
+//Light Text Style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.light,
+    color,
+  );
+}
+
+//Bold Text Style
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.bold,
+    color,
+  );
+}
+
+//Medium Text Style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.medium,
+    color,
+  );
+}
+
+//Semi-Bold Text Style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
